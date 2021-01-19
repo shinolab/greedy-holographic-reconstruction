09322c790f385e9cdf8b034800e1f1843fca617a
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-01-19 11:49:44 +0900

    fix: time

--- ghr/examples/time.rs
+++ ghr/examples/time.rs
@@ -4,7 +4,7 @@
  * Created Date: 09/07/2020
  * Author: Shun Suzuki
  * -----
- * Last Modified: 18/01/2021
+ * Last Modified: 19/01/2021
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2020 Hapis Lab. All rights reserved.
@@ -12,11 +12,12 @@
  */
 
 use ghr::{
-    calculator::{Calculator, CpuCalculator},
+    buffer::{generator::Amplitude, BufferBuilder},
+    calculator::{Calculate, Calculator, CpuCalculator},
     optimizer::*,
     vec_utils::*,
     wave_source::WaveSource,
-    Float, PI,
+    Float, Vector3, PI,
 };
 
 use rand::prelude::*;
@@ -26,155 +27,233 @@ use std::time::Instant;
 const SOURCE_SIZE: Float = 10.0;
 const WAVE_LENGTH: Float = 8.5;
 
-macro_rules! iterate {
-    ($x: block, $iter: tt) => {{
-        let start = Instant::now();
-        for _ in 0..$iter {
-            $x;
+fn calc_p1(focus: Vector3, n_sqrt: usize) -> Float {
+    let mut calculator = CpuCalculator::new();
+    calculator.set_wave_number(2.0 * PI / WAVE_LENGTH);
+
+    let mut transducers = Vec::new();
+    for y in 0..n_sqrt {
+        for x in 0..n_sqrt {
+            let pos = [SOURCE_SIZE * x as Float, SOURCE_SIZE * y as Float, 0.];
+            let phase = (norm(sub(pos, focus)) % WAVE_LENGTH) / WAVE_LENGTH;
+            transducers.push(WaveSource::new(pos, 1.0, -2.0 * PI * phase));
         }
-        start.elapsed().as_millis() / $iter
-    }};
+    }
+    calculator.add_wave_sources(&transducers);
+    let mut buffer = BufferBuilder::new()
+        .x_at(focus[0])
+        .y_at(focus[1])
+        .z_at(focus[2])
+        .resolution(1.)
+        .generate::<Amplitude>();
+
+    buffer.calculate(&calculator);
+    buffer.buffer()[0] as f64
 }
 
-macro_rules! measure {
-    ($opt: ty, $m: tt, $n_sqrt: tt) => {{
-        let focus_z = 150.0;
-        let focal_pos = [
-            SOURCE_SIZE * ($n_sqrt - 1) as Float / 2.0,
-            SOURCE_SIZE * ($n_sqrt - 1) as Float / 2.0,
-            focus_z,
-        ];
-        let obs_range = 100.0;
+fn generate_test_set(
+    center: Vector3,
+    obs_range: Float,
+    n_sqrt: usize,
+    m: usize,
+    iter: usize,
+) -> (Vec<Vec<Vector3>>, Vec<Vec<Float>>) {
+    let p1 = calc_p1(center, n_sqrt);
 
-        let mut calculator = CpuCalculator::new();
-        calculator.set_wave_number(2.0 * PI / WAVE_LENGTH);
-
-        let mut transducers = Vec::new();
-        for y in 0..$n_sqrt {
-            for x in 0..$n_sqrt {
-                let pos = [SOURCE_SIZE * x as Float, SOURCE_SIZE * y as Float, 0.];
-                transducers.push(WaveSource::new(pos, 0.0, 0.0));
-            }
+    let mut rng = rand::thread_rng();
+    let mut foci_set = Vec::new();
+    let mut amps_set = Vec::new();
+    for _ in 0..iter {
+        let mut foci = Vec::with_capacity(m);
+        for _ in 0..m {
+            foci.push(add(
+                center,
+                [
+                    (rng.gen::<Float>() - 0.5) * obs_range,
+                    (rng.gen::<Float>() - 0.5) * obs_range,
+                    0.0,
+                ],
+            ));
         }
-        calculator.add_wave_sources(&transducers);
+        let mut amps = Vec::with_capacity(foci.len());
+        let amp = p1 / (m as f64).sqrt();
+        for _ in 0..foci.len() {
+            amps.push(amp);
+        }
+        foci_set.push(foci);
+        amps_set.push(amps);
+    }
+    (foci_set, amps_set)
+}
 
-        let mut rng = rand::thread_rng();
-        let millis = iterate!(
-            {
-                for source in calculator.wave_sources() {
-                    source.amp = 0.0;
-                    source.phase = 0.0;
-                }
-                let mut target_pos = Vec::with_capacity($m);
-                for _ in 0..$m {
-                    target_pos.push(add(
-                        focal_pos,
-                        [
-                            (rng.gen::<Float>() - 0.5) * obs_range,
-                            (rng.gen::<Float>() - 0.5) * obs_range,
-                            0.0,
-                        ],
-                    ));
-                }
-                let mut amps = Vec::with_capacity(target_pos.len());
-                for _ in 0..target_pos.len() {
-                    amps.push(1.0);
-                }
-                let optimizer = <$opt>::new(target_pos.clone(), amps.clone(), WAVE_LENGTH as f64);
-                optimizer.optimize(calculator.wave_sources(), false);
-            },
-            100
-        );
-        millis
-    }};
+fn measure_time<T: Optimizer>(
+    opt: T,
+    name: &str,
+    n_sqrt: usize,
+    m: usize,
+    foci_set: &[Vec<Vector3>],
+    amps_set: &[Vec<Float>],
+) {
+    let mut opt = opt;
+
+    let mut calculator = CpuCalculator::new();
+    calculator.set_wave_number(2.0 * PI / WAVE_LENGTH);
+
+    let mut transducers = Vec::new();
+    for y in 0..n_sqrt {
+        for x in 0..n_sqrt {
+            let pos = [SOURCE_SIZE * x as Float, SOURCE_SIZE * y as Float, 0.];
+            transducers.push(WaveSource::new(pos, 0.0, 0.0));
+        }
+    }
+    calculator.add_wave_sources(&transducers);
+
+    let mut elasped = Vec::new();
+    for (foci, amps) in foci_set.iter().zip(amps_set.iter()) {
+        for source in calculator.wave_sources() {
+            source.amp = 0.0;
+            source.phase = 0.0;
+        }
+        opt.set_target_foci(foci);
+        opt.set_target_amps(amps);
+
+        let start = Instant::now();
+        opt.optimize(calculator.wave_sources());
+        elasped.push(start.elapsed().as_micros());
+    }
+
+    let mut wtr =
+        csv::Writer::from_path(format!("times/{}_M{}_N{}.csv", name, m, n_sqrt * n_sqrt)).unwrap();
+    write_data(&mut wtr, &elasped);
+    println!("\t{} done", name);
+}
+
+fn write_data<T: std::io::Write>(wtr: &mut csv::Writer<T>, data: &[u128]) {
+    for &v in data {
+        wtr.write_record(&[v.to_string()]).unwrap();
+    }
 }
 
 fn main() {
     let n_sqrt = 10;
-    let m_max_pow = 10;
-    let n_max_pow = 16;
+    let iter = 5;
 
-    // // GHRBF PHASE
-    // {
-    //     println!("Greedy Holographic Reconstruction with phase");
-    //     let file_path = "ghr_p.csv";
-    //     let mut wtr = csv::Writer::from_path(file_path).unwrap();
-    //     wtr.write_record(&["N", "M", "time[ms]"]).unwrap();
-    //     for i in 1..=m_max_pow {
-    //         let m = 1 << i;
-    //         println!("{}", m);
-    //         let millis = measure!(GreedyBruteForce, m, n_sqrt);
-    //         wtr.write_record(&[
-    //             (n_sqrt * n_sqrt).to_string(),
-    //             (m).to_string(),
-    //             millis.to_string(),
-    //         ])
-    //         .unwrap();
-    //     }
-    // }
+    let m_max_pow = 6;
+    let n_max_pow = 5;
 
-    // // Long
-    // println!("Long te al, 2014");
-    // let file_path = "long.csv";
-    // let mut wtr = csv::Writer::from_path(file_path).unwrap();
-    // wtr.write_record(&["N", "M", "time[ms]"]).unwrap();
-    // for i in 1..=m_max_pow {
-    //     let m = 1 << i;
-    //     println!("{}", m);
-    //     let millis = measure!(Long, m, n_sqrt);
-    //     wtr.write_record(&[
-    //         (n_sqrt * n_sqrt).to_string(),
-    //         (1 << i).to_string(),
-    //         millis.to_string(),
-    //     ])
-    //     .unwrap();
-    // }
+    let focus_z = 150.0;
+    let center = [
+        SOURCE_SIZE * (n_sqrt - 1) as Float / 2.0,
+        SOURCE_SIZE * (n_sqrt - 1) as Float / 2.0,
+        focus_z,
+    ];
 
-    // // HORN
-    // println!("HORN");
-    // let file_path = "horn.csv";
-    // let mut wtr = csv::Writer::from_path(file_path).unwrap();
-    // wtr.write_record(&["N", "M", "time[ms]"]).unwrap();
-    // for i in 1..=m_max_pow {
-    //     let m = 1 << i;
-    //     println!("{}", m);
-    //     let millis = measure!(Horn, m, n_sqrt);
-    //     wtr.write_record(&[
-    //         (n_sqrt * n_sqrt).to_string(),
-    //         (1 << i).to_string(),
-    //         millis.to_string(),
-    //     ])
-    //     .unwrap();
-    // }
+    std::fs::create_dir("times").unwrap_or(());
 
-    // LM
-    println!("Levenberg-Marquardt");
-    let file_path = "gbs_M.csv";
-    let mut wtr = csv::Writer::from_path(file_path).unwrap();
-    wtr.write_record(&["N", "M", "time[ms]"]).unwrap();
     for i in 1..=m_max_pow {
         let m = 1 << i;
-        println!("m: {}", m);
-        let millis = measure!(GreedyBruteForce, m, n_sqrt);
-        wtr.write_record(&[
-            (n_sqrt * n_sqrt).to_string(),
-            (1 << i).to_string(),
-            millis.to_string(),
-        ])
-        .unwrap();
+
+        println!("testing: M={}, N={}", m, n_sqrt * n_sqrt);
+
+        let (foci_set, amps_set) = generate_test_set(center, 100.0, m, n_sqrt, iter);
+
+        measure_time(
+            GreedyBruteForce::new(16, 1, WAVE_LENGTH),
+            "gbf_16_1",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
+
+        measure_time(
+            Horn::new(1000, 1e-3, 0.9, WAVE_LENGTH),
+            "horn",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
+
+        measure_time(
+            Long::new(1.0, WAVE_LENGTH),
+            "long",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
+
+        measure_time(
+            LM::new(1e-8, 1e-8, 1e-3, 200, WAVE_LENGTH),
+            "lm",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
+
+        measure_time(
+            GSPAT::new(100, WAVE_LENGTH),
+            "gspat",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
     }
 
-    // LM
-    println!("Levenberg-Marquardt");
-    let file_path = "gbs_N.csv";
-    let mut wtr = csv::Writer::from_path(file_path).unwrap();
-    wtr.write_record(&["N", "M", "time[ms]"]).unwrap();
-    for i in 1..=n_max_pow {
-        let n = 2 * i;
-        let m = 128;
-        println!("n: {}", n * n);
-        let millis = measure!(GreedyBruteForce, 128, n);
-        wtr.write_record(&[(n * n).to_string(), m.to_string(), millis.to_string()])
-            .unwrap();
+    let m = 64;
+    for i in 2..=n_max_pow {
+        let n_sqrt = 1 << i;
+
+        println!("testing: M={}, N={}", m, n_sqrt * n_sqrt);
+
+        let (foci_set, amps_set) = generate_test_set(center, 100.0, m, n_sqrt, iter);
+
+        measure_time(
+            GreedyBruteForce::new(16, 1, WAVE_LENGTH),
+            "gbf_16_1",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
+
+        measure_time(
+            Horn::new(1000, 1e-3, 0.9, WAVE_LENGTH),
+            "horn",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
+
+        measure_time(
+            Long::new(1.0, WAVE_LENGTH),
+            "long",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
+
+        measure_time(
+            LM::new(1e-8, 1e-8, 1e-3, 200, WAVE_LENGTH),
+            "lm",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
+
+        measure_time(
+            GSPAT::new(100, WAVE_LENGTH),
+            "gspat",
+            n_sqrt,
+            m,
+            &foci_set,
+            &amps_set,
+        );
     }
 }
