c1cff80036ff7cddeb5012ea588add83f794c190
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-01-20 10:41:21 +0900

    add: mac

--- ghr-capi/build.rs
+++ ghr-capi/build.rs
@@ -4,7 +4,7 @@
  * Created Date: 01/01/1970
  * Author: Shun Suzuki
  * -----
- * Last Modified: 19/01/2021
+ * Last Modified: 20/01/2021
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Hapis Lab. All rights reserved.
@@ -13,6 +13,12 @@
 
 #[cfg(target_os = "linux")]
 fn main() {
-    println!("cargo:rustc-link-search=/usr/local/lib/");
     println!("cargo:rustc-link-search=/opt/openblas/lib");
+}
+
+#[cfg(target_os = "macos")]
+fn main() {
+    println!("cargo:rustc-link-search=/usr/local/opt/openblas/lib");
+    println!("cargo:rustc-link-search=/usr/local/opt/lapack/lib");
+    println!("cargo:rustc-link-search=/usr/local/lib/gcc/10/");
 }
